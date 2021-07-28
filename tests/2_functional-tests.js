6a07b7f3d1885542d9ee3797fdf4bd0699829fe4
Alexis Montes <montes.alexis25@gmail.com>
2021-07-27 22:01:34 -0400

    fixed error with suite execution

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -85,28 +85,31 @@ suite("Functional Tests with Zombie.js", function () {
 
   suite('"Famous Italian Explorers" form', function () {
     // #5
-    test('submit "surname" : "Colombo" - write your e2e test...', function (done) {
-      browser.fill("surname", "Colombo").pressButton("submit", function () {
+    test('submit "surname" : "Colombo" - write your e2e test...', function(done) {
+      
+      browser.fill('surname', 'Colombo').pressButton('submit', function() {
+        
         browser.assert.success();
         browser.assert.text('span#name', 'Cristoforo');
         browser.assert.text('span#surname', 'Colombo');
-        browser.assert.elements('span#dates', 1);
-        done();
+        browser.assert.element('span#dates', 1);
+    
+        done(); 
       });
     });
     
     // #6
-    test('submit "surname" : "Vespucci" - write your e2e test...', function (done) {
-      browser.fill("surname", "Vespucci").pressButton("submit", function () {
+    test('submit "surname" : "Vespucci" - write your e2e test...', function(done) {
+      
+      browser.fill('surname', 'Vespucci').pressButton('submit', function() {
+        
         browser.assert.success();
-        browser.assert.text('span#name', 'Cristoforo');
-        browser.assert.text('span#surname', 'Colombo');
-        browser.assert.elements('span#dates', 1);
-
-      done();
-    })
-  })
-
-
-})
-})
+        browser.assert.text('span#name', 'Amerigo');
+        browser.assert.text('span#surname', 'Vespucci');
+        browser.assert.element('span#dates', 1);
+    
+        done();
+      });
+    });
+  });
+});
